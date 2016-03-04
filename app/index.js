9c13e033be31c67384499d8d5f88dd8e14ff3fb6
Stian Håklev <shaklev@gmail.com>
2016-03-04 13:33:11 -0500

    Initial

---
+++ app/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { render } from 'react-dom'
+import Dropdown from 'react-dropdown'
+require('../node_modules/react-dropdown/dist/style.css')
+
+import { defaultState, states, routes } from '../config'
+
+// set up tracking of URL bar
+const navigated = () => {
+  const newroute = window.location.hash.slice(2)
+}
+
+export const initialize = () => {
+  window.addEventListener('hashchange', navigated, false)
+  navigated()
+}
+
+export const changeRoute = (route) => {
+  window.setTimeout( () => window.location.hash = '/' + route, 0)
+}
+
+class Route extends React.Component {
+  state = {
+    curstate: defaultState, 
+    states: states, 
+    routes: routes,
+    route: {title: "Default", component: () => <div></div>},
+    showmenu: true
+  }
+
+  setRoute = (route) => {
+    this.setState({route: route})
+  }
+  
+  changeState = ( { label } ) => {
+    const newstate = this.state.states.filter(e => e.title == label)[0].state
+    this.setState({curstate: newstate})
+  }
+
+  routeMenu = this.state.routes.map(e => ({label: e.title, value: e.title}))
+  stateMenu = this.state.states.map(e => ({label: e.title, value: e.title}))
+
+  changeRoute = ( { label } ) => {
+    const newroute = this.state.routes.filter(e => e.title == label)[0].component
+    this.setState({route: {title: label, component: newroute}})
+  }
+
+  menu = <div style={{height: '50px', width: '100vh', margin: '0px', background: '#ffc'}}>
+    <b>Settings</b> 
+    <div style={{top: 0, left: 200, position: 'absolute'}}>
+      <Dropdown options={this.routeMenu} placeholder='Select route' onChange={this.changeRoute} />
+    </div> 
+    <div style={{top: 0, left: 500, position: 'absolute'}}>
+      <Dropdown options={this.stateMenu} placeholder='Select state' onChange={this.changeState} />
+    </div>
+    <div style={{top: 0, right: 10, position: 'absolute'}}><span onClick={() => this.setState({showmenu: false})}>x</span></div>
+    </div>
+
+  render() { 
+    return (
+    <div>
+    <div style={{position: "absolute", top: 0}}>
+    {this.state.showmenu ? 
+      this.menu : 
+      <span onClick={() => this.setState({showmenu: true})}>&#9776;</span>
+    }
+    </div>
+    <div style={{position: 'absolute', top: '55px'}}>
+
+    {React.createElement(this.state.route.component, {state: this.state.curstate})}
+    </div>
+    </div>
+  ) }
+}
+
+render( <Route />, document.getElementById('root'))
